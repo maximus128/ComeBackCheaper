e1189fd94390b3efdd3ca2f804751537a8f5d4f9
Maksim Litviniuk <maksim.litviniuk@gmail.com>
2022-08-09 21:38:29 +0200

    adding dependencies: 1.Spring Boot Starter Web 2.Spring Boot Starter Data JPA 3.PostgreSQL JDBC Driver 4.Project Lombok

---
+++ src/main/java/com/example/comebackcheaper/ComeBackCheaperApplication.java
@@ -0,0 +1,13 @@
+package com.example.comebackcheaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeBackCheaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComeBackCheaperApplication.class, args);
+	}
+
+}
